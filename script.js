aebae0d22ef5fb756449b63b42c6e419ba3aea72
Ramkrishna Roy <76205677+Ramkrishna307@users.noreply.github.com>
2024-01-25 10:44:12 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,17 @@
 const extendHex = (shortHex) => {
   // write your code here
+// Remove the "#" symbol if present
+  shortHex = shortHex.replace(/^#/, '');
+
+  // Split the short hex code into individual color components
+  const r = shortHex[0];
+  const g = shortHex[1];
+  const b = shortHex[2];
+
+  // Extend each color component to the full hex format
+  const fullHex = `#${r}${r}${g}${g}${b}${b}`;
+
+  return fullHex.toUpperCase(); // Convert to uppercase for consistency
 };
 
 // Do not change the code below.
